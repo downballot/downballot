bbcd0a72adc9c21d272a5ec30358da4fa59deebc
Douglas Danger Manley <doug.manley@gmail.com>
2026-06-06 12:28:39 -0400

    Fix

--- downballotapi/types-user-group.go
+++ downballotapi/types-user-group.go
@@ -5,11 +5,6 @@ type ListUserGroupsResponse struct {
 	UserGroups []*UserGroup `json:"user_groups"`
 }
 
-// GetUserGroupResponse is the response from getting a user group.
-type GetUserGroupResponse struct {
-	UserGroup *UserGroup `json:"user_group"`
-}
-
 // UserGroup is a group that the user is in.
 type UserGroup struct {
 	Group
