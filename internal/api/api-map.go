95745ea0de656381a92ca4d379858003d3462af1
Douglas Danger Manley <doug.manley@gmail.com>
2026-08-06 11:53:26 -0400

    Address fixes

--- internal/api/api-map.go
+++ internal/api/api-map.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/downballot/downballot/downballotapi"
 	"github.com/downballot/downballot/internal/api/downballotwrapper"
+	"github.com/downballot/downballot/internal/normalize"
 	"github.com/tekkamanendless/restfulwrapper"
 	"googlemaps.github.io/maps"
 )
@@ -27,8 +28,14 @@ func (a *API) PostMapGeocode(ctx context.Context, meta PostMapGeocodeMetadata) (
 		return output, fmt.Errorf("could not create Google Maps client: %w", err)
 	}
 
+	slog.DebugContext(ctx, fmt.Sprintf("Initial address: %s", meta.Body.Address))
+
+	// Pre-process the address.
+	address := normalize.PreProcessAddress(meta.Body.Address)
+	slog.DebugContext(ctx, fmt.Sprintf("Pre-processed address: %s", address))
+
 	geocodeRequest := &maps.GeocodingRequest{
-		Address: meta.Body.Address,
+		Address: address,
 	}
 	results, err := client.Geocode(ctx, geocodeRequest)
 	if err != nil {
@@ -41,8 +48,14 @@ func (a *API) PostMapGeocode(ctx context.Context, meta PostMapGeocodeMetadata) (
 			slog.DebugContext(ctx, fmt.Sprintf("Address component: %+v", addressComponent))
 		}
 
+		postProcessedAddress := normalize.PostProcessAddress(result.FormattedAddress)
+		slog.DebugContext(ctx, fmt.Sprintf("Post-processed address: %s", postProcessedAddress))
+
 		newRecord := downballotapi.MapGeocodeResult{
-			FormattedAddress: result.FormattedAddress,
+			InitialAddress:       meta.Body.Address,
+			PreProcessedAddress:  address,
+			FormattedAddress:     result.FormattedAddress,
+			PostProcessedAddress: postProcessedAddress,
 			Coordinates: downballotapi.Coordinates{
 				Latitude:  result.Geometry.Location.Lat,
 				Longitude: result.Geometry.Location.Lng,
--- internal/normalize/location.go
+++ internal/normalize/location.go
@@ -4,11 +4,73 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"regexp"
+	"strings"
 
 	"github.com/downballot/downballot/downballotapi"
 	"github.com/downballot/downballot/internal/cleanup"
 	"googlemaps.github.io/maps"
 )
+
+type processExpression struct {
+	regex       *regexp.Regexp
+	replacement string
+}
+
+var preprocessExpressions = []processExpression{
+	// Fix the Christina Mill apartments.
+	// They no longer use apartment numbers.
+	{
+		regex:       regexp.MustCompile(`^100 christina mill dr apt ([0-9]+), newark, de 19711`),
+		replacement: "$1 christina mill dr, newark, de 19711",
+	},
+	{
+		regex:       regexp.MustCompile(`^100 christina mill dr #([0-9]+), newark, de 19711`),
+		replacement: "$1 christina mill dr, newark, de 19711",
+	},
+	// Convert room numbers to apartment numbers.
+	{
+		regex:       regexp.MustCompile(`^([^,]+) rm ([^ ]+), `),
+		replacement: "$1 apt $2, ",
+	},
+}
+
+var postprocessExpressions = []processExpression{
+	// Fix single-letter apartment numbers.
+	// Basically, we just want to put a "#" in front of them.
+	{
+		regex:       regexp.MustCompile(`^([^,]+) ([a-zA-Z]), `),
+		replacement: "$1 #$2, ",
+	},
+	// Fix unconverted apartment numbers.
+	// We just want to put a "#" in front of them, instead of "Apt".
+	{
+		regex:       regexp.MustCompile(`^([^,]+) Apt ([0-9]+[a-zA-Z0-9]*), `),
+		replacement: "$1 #$2, ",
+	},
+}
+
+// PreProcessAddress pre-processes the address.
+func PreProcessAddress(address string) string {
+	address = strings.ToLower(address)
+
+	for _, expression := range preprocessExpressions {
+		if expression.regex.MatchString(address) {
+			address = expression.regex.ReplaceAllString(address, expression.replacement)
+		}
+	}
+	return address
+}
+
+// PostProcessAddress post-processes the address.
+func PostProcessAddress(address string) string {
+	for _, expression := range postprocessExpressions {
+		if expression.regex.MatchString(address) {
+			address = expression.regex.ReplaceAllString(address, expression.replacement)
+		}
+	}
+	return address
+}
 
 // Location normalizes the location of a person.
 func Location(ctx context.Context, mapsClient *maps.Client, nccClient *NewCastleCountyGIS, normalizePerson *downballotapi.NormalizePerson) error {
@@ -16,6 +78,12 @@ func Location(ctx context.Context, mapsClient *maps.Client, nccClient *NewCastle
 		return nil
 	}
 	address := *normalizePerson.OldFields["residential_address"]
+
+	slog.DebugContext(ctx, fmt.Sprintf("Initial address: %s", address))
+
+	// Pre-process the address.
+	address = PreProcessAddress(address)
+	slog.DebugContext(ctx, fmt.Sprintf("Pre-processed address: %s", address))
 
 	// Google Maps.
 	{
@@ -38,10 +106,11 @@ func Location(ctx context.Context, mapsClient *maps.Client, nccClient *NewCastle
 					coordinates := fmt.Sprintf("%f,%f", result.Geometry.Location.Lat, result.Geometry.Location.Lng)
 					normalizePerson.NewFields["coordinates"] = &coordinates
 
-					address, err := cleanup.Address(result.FormattedAddress)
+					newAddress, err := cleanup.Address(result.FormattedAddress)
 					if err != nil {
 						slog.WarnContext(ctx, fmt.Sprintf("could not cleanup address: %s", err))
 					} else {
+						address = newAddress
 						normalizePerson.NewFields["residential_address"] = &address
 					}
 					break
@@ -49,6 +118,10 @@ func Location(ctx context.Context, mapsClient *maps.Client, nccClient *NewCastle
 			}
 		}
 	}
+
+	// Post-process the address.
+	address = PostProcessAddress(address)
+	slog.DebugContext(ctx, fmt.Sprintf("Post-processed address: %s", address))
 
 	// New Castle County.
 	if normalizePerson.NewFields["coordinates"] != nil && *normalizePerson.NewFields["coordinates"] != "" {
